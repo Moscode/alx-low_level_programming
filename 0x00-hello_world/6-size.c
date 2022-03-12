6b3320cdc4683657063a0ed8c585de9d702a8ea0
Odunosh Moses <Odunoshomoses@gmail.com>
2022-03-12 15:11:26 +0000

    Reprint each statement with a printf command each

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -7,15 +7,10 @@
  */
 int main(void)
 {
-	printf("Size of a char:%d byte(s)\n
-		Size of an int:%d byte(s)\n
-		Size of a long int:%d byte(s)\n
-		Size of a long long int:%d byte(s)\n
-		Size of a float:%d byte(s)",
-		sizeof(char),
-		sizeof(int),
-		sizeof(long int),
-		sizeof(long long int),
-		sizeof(float));
+	printf("Size of a char:%lu byte(s)\n", sizeof(char));
+	printf("Size of an int:%lu byte(s)\n", sizeof(int));
+	printf("Size of a long int:%li byte(s)\n", sizeof(long int));
+	printf("Size of a long long int:%lu byte(s)\n", sizeof(long long int));
+	printf("Size of a float:%lu byte(s)\n", sizeof(float));
 	return (0);
 }
