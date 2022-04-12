b95081ab9a3ecc5f4282d2b4ad6c734630a07e36
Moscode <odunoshomoses@gmail.com>
2022-04-12 15:17:07 -0700

    alphabets task

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,15 @@
+#include <stdlib.h>
+#include <time.h>
+/* more headers goes there */
+#include <stdio.h>
+/* betty style doc for function main goes there */
+int main(void)
+{
+	int n;
+
+	for (n = 'a'; n <= 'z'; n++)
+	{
+		putchar(n);
+	}
+	return (0);
+}
