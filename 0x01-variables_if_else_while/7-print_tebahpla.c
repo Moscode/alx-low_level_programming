4edf902425f1c965815a70a1ab061851e04f09c8
Moscode <odunoshomoses@gmail.com>
2022-04-13 05:27:27 -0700

    print a - z in reverse order

---
+++ 0x01-variables_if_else_while/7-print_tebahpla.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	int n;
+
+	for (n = 'z'; n >= 'a'; n--)
+	{
+		putchar(n);
+	}
+	putchar('\n');
+	return (0);
+}
