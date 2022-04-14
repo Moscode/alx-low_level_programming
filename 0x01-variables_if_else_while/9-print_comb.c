75366ffdb0d994c41b46a42dc6cbc4694c88eee8
Moscode <odunoshomoses@gmail.com>
2022-04-14 10:54:41 -0700

    Avoiding adding , and space after integer 9

--- 0x01-variables_if_else_while/9-print_comb.c
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -11,8 +11,11 @@ int main(void)
 	for (n = 0; n <= 9; n++)
 	{
 		putchar(n + '0');
-		putchar(',');
-		putchar(' ');
+		if (n != 9)
+		{
+			putchar(',');
+			putchar(' ');
+		}
 	}
 	putchar('\n');
 	return (0);
