bc6b41361790f0ded3aa34dbdf2dd1148fabfd75
Moscode <odunoshomoses@gmail.com>
2022-04-14 10:15:32 -0700

    Task 8 - putchar Hexadecimal using 2 for loops

---
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	int n;
+
+	for (n = 0; n <= 9; n++)
+	{
+		putchar(n + '0');
+	}
+	for (n = 'a'; n <= 'f'; n++)
+	{
+		putchar(n);
+	}
+	putchar('\n');
+	return (0);
+}
