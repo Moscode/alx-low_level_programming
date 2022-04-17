c3d0b181b831188d0fd8b6fad437211b14d50dc8
Moscode <odunoshomoses@gmail.com>
2022-04-17 03:45:55 -0700

    Using different thought process to solve the problem

--- 0x01-variables_if_else_while/102-print_comb5.c
+++ 0x01-variables_if_else_while/102-print_comb5.c
@@ -6,34 +6,22 @@
  */
 int main(void)
 {
-	int i, j, k, l;
+	int i, j;
 
-	for (i = 0; i <= 9; i++)
+	for (i = 0; i <= 98; i++)
 	{
-		for (j = 0; j <= 9; j++)
+		for (j = i + 1; j <= 99; j++)
 		{
-			for (k = 0; k <= 9; k++)
-			{
-				for (l = 0; l <= 9; l++)
-				{
-					if ((i != k) || (j != l))
-					{
-					if ((i < k) || (j < l))
-					{
-						putchar(i + '0');
-						putchar(j + '0');
-						putchar(' ');
-						putchar(k + '0');
-						putchar(l + '0');
-					if ((i != 9) || (j != 8) || (k != 9) || (l != 9))
-					{
-						putchar(',');
-						putchar(' ');
-					}
-					}
-					}
-				}
-			}
+			putchar((i / 10) + '0');
+			putchar((i % 10) + '0');
+			putchar(' ');
+			putchar((j / 10) + '0');
+			putchar((j % 10) + '0');
+
+			if ((i == 98) && (j == 99))
+				continue;
+			putchar(',');
+			putchar(' ');
 		}
 	}
 	putchar('\n');
