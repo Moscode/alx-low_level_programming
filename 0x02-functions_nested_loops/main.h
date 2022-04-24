d00a8159684456831f534997d4b3da496e2c14c9
Moscode <odunoshomoses@gmail.com>
2022-04-24 05:12:03 -0700

    Task  - print all alphabets in lowercase

--- 0x02-functions_nested_loops/main.h
+++ 0x02-functions_nested_loops/main.h
@@ -1,9 +1,9 @@
 #ifndef MAIN_H
 #define MAIN_H
-//Print to standard output using _putchar()
+/* Print to standard output using _putchar() */
 int _putchar(char c);
 
-//Print alphabet on the standard output
+/* Print alphabet on the standard output */
 void print_alphabet(void);
 
 #endif
