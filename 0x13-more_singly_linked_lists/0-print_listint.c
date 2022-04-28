226fd7d4e1d0339d6f12e204b55f58759189885a
Moscode <odunoshomoses@gmail.com>
2022-04-27 20:43:29 -0700

    Task 0 - print list of int

---
+++ 0x13-more_singly_linked_lists/0-print_listint.c
@@ -0,0 +1,20 @@
+#include "lists.h"
+
+/**
+ * print_listint - prints all the elements of a listint_t list
+ * @h: pointer to head
+ * Return: the number of nodes
+ */
+
+size_t print_listint(const listint_t *h)
+{
+	size_t list_count = 0;
+
+	while (h != NULL)
+	{
+		printf("%d\n", h->n);
+		h = h->next;
+		++list_count;
+	}
+	return (list_count);
+}
