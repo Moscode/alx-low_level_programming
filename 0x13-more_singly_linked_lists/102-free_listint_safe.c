96eb71bd43b927f8399d594ac2c935c6fbc60951
Moscode <odunoshomoses@gmail.com>
2022-04-27 21:09:04 -0700

    Task 13 - Free linked list and output the size of linked list

---
+++ 0x13-more_singly_linked_lists/102-free_listint_safe.c
@@ -0,0 +1,64 @@
+#include "lists.h"
+
+/**
+ * _ra - reallocates memory for an array of pointers
+ * to the nodes in a linked list
+ * @list: the old list to append
+ * @size: size of the new list (always one more than the old list)
+ * @new: new node to add to the list
+ *
+ * Return: pointer to the new list
+ */
+
+listint_t **_ra(listint_t **list, size_t size, listint_t *new)
+{
+	listint_t **new_list;
+	size_t i;
+
+	new_list = malloc(size * sizeof(listint_t *));
+	if (new_list == NULL)
+	{
+		free(list);
+		exit(98);
+	}
+	for (i = 0; i < size - 1; i++)
+		new_list[i] = list[i];
+	new_list[i] = new;
+	free(list);
+	return (new_list);
+}
+
+/**
+ * free_listint_safe - frees a linked list.
+ * @head: double pointer to the head node
+ *
+ * Return: the number of nodes in the list
+ */
+size_t free_listint_safe(listint_t **head)
+{
+	size_t i, num = 0;
+	listint_t **list = NULL;
+	listint_t *next;
+
+	if (head == NULL || *head == NULL)
+		return (num);
+	while (*head != NULL)
+	{
+		for (i = 0; i < num; i++)
+		{
+			if (*head == list[i])
+			{
+				*head = NULL;
+				free(list);
+				return (num);
+			}
+		}
+		num++;
+		list = _ra(list, num, *head);
+		next = (*head)->next;
+		free(*head);
+		*head = next;
+	}
+	free(list);
+	return (num);
+}
