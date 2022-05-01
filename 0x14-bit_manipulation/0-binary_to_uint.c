6c464f6cd6817984c7b117764972e8069b899540
Moscode <odunoshomoses@gmail.com>
2022-05-01 04:06:44 -0700

    Debugging 0-binary_to_uint.c

--- 0x14-bit_manipulation/0-binary_to_uint.c
+++ 0x14-bit_manipulation/0-binary_to_uint.c
@@ -1,56 +1,51 @@
 #include "main.h"
 
 /**
- * raiseToPower - Compute value to the power of pow
- *@value: main value
- *@pow: exponent
+ * _pow - calculates a raised to the power b
+ * @a: base number
+ * @b: exponent
  *
- *Return: total computation value Always > 0 (Success)
+ * Return: number
  */
 
-int raiseToPower(int value, int pow)
+int _pow(int a, int b)
 {
-	if (pow < 0)
+	if (b < 0)
 		return (-1);
-	else if (pow == 0)
+	if (b == 0)
 		return (1);
-	else
-		return (value * raiseToPower(value, pow - 1));
+	return (a * _pow(a, b - 1));
 }
 
 /**
- * binary_to_unit - convert from base 2 to base 10
+ * binary_to_uint - converts a binary number to an unsigned int
+ * @b: pointer to a string of 0 and 1 chars
  *
- *@b: pointer to a string of 0(s) and 1(s) chars
- *
- * Return: converted number in base 10 or 0 if b is Null or b > 1
- */
-
+ * Return: converted number or 0 if b is NULL or have chars that aren't 0 or 1
+*/
 
 unsigned int binary_to_uint(const char *b)
 {
-	unsigned int result;
-	int length, power;
+	unsigned int decimal;
+	int len, power;
 
 	if (b == NULL)
 		return (0);
 
+	len = 0;
+	while (b[len] != '\0')
+		len++;
 
-	length = 0;
-	while (b[length] != '\0')
-		length++;
-
-
-	result = 0;
+	decimal = 0;
 	power = 0;
-	while (length-- && length >= 0)
+	while (len-- && len >= 0)
 	{
-		if (b[length] == '1')
-			result += raiseToPower(2, power);
-		else if (b[length] != '0')
+		if (b[len] == '1')
+			decimal += _pow(2, power);
+		else if (b[len] != '0')
 			return (0);
 		power++;
 	}
 
-	return (result);
+	return (decimal);
 }
