240637aaf18fe677409b1a4c7f62b7e3113d6227
Moscode <odunoshomoses@gmail.com>
2022-05-01 03:59:11 -0700

    Added unistd library to the main.h

--- 0x14-bit_manipulation/main.h
+++ 0x14-bit_manipulation/main.h
@@ -4,13 +4,6 @@
 #include <stdlib.h>
 #include <unistd.h>
 
-/**
- * binary_to_uint
- *
- * @b: pointer to a character
- */
-
 int _putchar(char c);
 unsigned int binary_to_uint(const char *b);
-
 #endif /* _MAIN_H_ */
