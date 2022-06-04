d92fd3ed86dc469c12b12e0fed8ae85aa65a0238
Moscode <odunoshomoses@gmail.com>
2022-06-04 08:48:43 -0700

    [feat]: adding two numbers together

---
+++ 0x02-functions_nested_loops/10-add.c
@@ -0,0 +1,14 @@
+/**
+ * add - add two numbers together
+ * @a: first number
+ * @b: second number
+ * Return: Always number (Success)
+ */
+
+int add(int a, int b)
+{
+	int res;
+
+	res = a + b;
+	return (res);
+}
