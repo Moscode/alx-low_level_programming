03f7c120cf0b4de96b08ed2ba5655190007dd52f
Moscode <odunoshomoses@gmail.com>
2022-06-05 08:33:36 -0700

    [fix]: made to be betty compliance

--- 0x02-functions_nested_loops/11-print_to_98.c
+++ 0x02-functions_nested_loops/11-print_to_98.c
@@ -9,14 +9,14 @@
  */
 void print_to_98(int n)
 {
-	while(n < 98)
+	while (n < 98)
 	{
 		printf("%d, ", n);
 		n++;
 	}
-	if(n > 98)
+	if (n > 98)
 	{
-		while(n > 98)
+		while (n > 98)
 		{
 			printf("%d, ", n);
 			n--;
